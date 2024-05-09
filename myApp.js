51ab3b650d7aae5a4895daf0800f23196c31b473
Tim Atobatele <dev.atobatele@gmail.com>
2024-05-09 17:38:05 +0000

    Completed : Use model.find() to Search Your Database

--- myApp.js
+++ myApp.js
@@ -31,8 +31,11 @@ const createManyPeople = (arrayOfPeople, done) => {
   });
 };
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+var findPeopleByName = function(personName, done) {
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
 const findOneByFood = (food, done) => {
